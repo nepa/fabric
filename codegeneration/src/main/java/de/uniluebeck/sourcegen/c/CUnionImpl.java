5632f82c50cdc0a390db4a6b237fbb469eff5bf6
Sascha Seidel <seidels@informatik.uni-luebeck.de>
2013-03-17 21:48:14 +0100

    Updated year in copyright header of source files.

--- codegeneration/src/main/java/de/uniluebeck/sourcegen/c/CUnionImpl.java
+++ codegeneration/src/main/java/de/uniluebeck/sourcegen/c/CUnionImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2012, Institute of Telematics (Dennis Pfisterer, Marco Wegner, Dennis Boldt,
+ * Copyright (c) 2010-2013, Institute of Telematics (Dennis Pfisterer, Marco Wegner, Dennis Boldt,
  * Sascha Seidel, Joss Widderich, et al.), University of Luebeck
  *
  * All rights reserved.
